e3702300fddabc54666921eb05abf42a51552e06
dyiar <austin.hl.blake@gmail.com>
2019-01-15 12:23:03 -0800

    finished stretch I think.

--- index.js
+++ index.js
@@ -55,7 +55,7 @@ server.put("/api/zoos/:id", (req, res) => {
     .update(changes)
     .then(count => {
       if (count) {
-        res.status(200).json(count);
+        res.status(200).send({count});
       } else {
         res.status(404).send({ error: "Data not found." });
       }
@@ -76,6 +76,70 @@ server.delete("/api/zoos/:id", (req, res) => {
     })
     .catch(() => res.status(500).send({ error: "Data couldn't be deleted." }));
 });
+
+server.get("/api/bears", (req, res) => {
+  db("bears")
+    .then(bear => {
+      res.status(200).send(bear);
+    })
+    .catch(() => res.status(500).send({ error: "Couldn't retrieve data." }));
+});
+
+server.get("/api/bears/:id", (req, res) => {
+  db("bears")
+    .where({ id: req.params.id })
+    .then(bear => {
+      if (bear.length) {
+        res.status(200).send(bear);
+      } else {
+        res.status(404).send({ error: "Data not found." });
+      }
+    })
+    .catch(() => res.status(500).send({ error: "Couldn't retrieve data." }));
+});
+
+server.post("/api/bears", (req, res) => {
+  db("bears")
+    .insert(req.body)
+    .then(ids => {
+      db("bears")
+        .where({ id: ids[0] })
+        .then(bear => {
+          res.status(201).send(bear);
+        });
+    })
+    .catch(() => res.status(500).send({ error: "Data couldn't be saved." }));
+});
+
+server.put("/api/bears/:id", (req, res) => {
+  const changes = req.body;
+
+  db("bears")
+    .where({ id: req.params.id })
+    .update(changes)
+    .then(count => {
+      if (count) {
+        res.status(200).json(count);
+      } else {
+        res.status(404).send({ error: "Data not found." });
+      }
+    })
+    .catch(() => res.status(500).send({ error: "Data couldn't be saved." }));
+});
+
+server.delete('/api/bears/:id', (req, res) => {
+  db('bears')
+  .where({ id: req.params.id })
+  .del()
+  .then(count => {
+    if (count) {
+      res.status(200).send({count})
+    } else {
+      res.status(404).send({ error: "Data not found." })
+    }
+  })
+  .catch(() => res.status(500).send({ error: "Data couldn't be deleted." }))
+})
 
 const port = 3300;
 server.listen(port, function() {
